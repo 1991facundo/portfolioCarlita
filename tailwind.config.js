a2d2eef189cfe83a8a383a0d96ef08f8fddfb56f
Facundo Fernandez <99399998+1991facundo@users.noreply.github.com>
2024-07-09 16:55:49 +0200

    tailwind bug fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    './index.html',
+    './**/*.{html,js}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        beige: '#E5E2C8',
+      },
+      fontFamily: {
+        amsterdam: ['"Amsterdam two"', 'sans-serif'],
+        questrial: ['Questrial', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
